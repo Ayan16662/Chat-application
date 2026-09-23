28f063534a94654fa78fa5d27a8c4df2fbb936da
Ayan16662 <ahmedayan88232@gmail.com>
2026-09-23 08:25:09 +0530

    u

--- Chatly/frontend/src/App.jsx
+++ Chatly/frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
@@ -8,11 +8,20 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 
 import useCurrentUser from "./Hooks/getCurrentUser";
+import {io} from "socket.io-client"
 
 const App = () => {
   useCurrentUser();
 
   const { userData } = useSelector((state) => state.user);
+  useEffect(()=>{
+    const socket = io("http://localhost:8000")
+    socket.on ("hello",(mess)=>{
+      console.log(mess)
+
+    })// recieve krege 
+
+  },[])
 
   console.log("Redux User:", userData);
 
